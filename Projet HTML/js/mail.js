e2dc6be72b62fefd0facb74d7d6e81d87b0b1452
amir <relation132@gmail.com>
2023-07-19 18:19:54 +0100

    V1

---
+++ Projet HTML/js/mail.js
@@ -0,0 +1,4 @@
+const userName = document.getElementById('userName');
+const mail = document.getElementById("mail");
+const phone = document.getElementById("phone");
+const objet = document.getElementById("objet");
